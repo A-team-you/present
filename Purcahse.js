49d689bcc8c0420534b2eebd2473113f3c48e80c
ddpl1 <111953655+ddpl1@users.noreply.github.com>
2023-06-07 10:16:34 +0900

    少し修正
    js

--- Purcahse.js
+++ Purcahse.js
@@ -1,31 +1,35 @@
-document.addEventListener('DOMContentLoaded', function() {
-    // button
-    const btn_clone = document.querySelector('.btn-add');  // 追加ボタン
-    const btn_remove = document.querySelector('.btn-remove');  // 削除ボタン
-  
-    // clone
-    btn_clone.addEventListener('click', function() {
-      const textFields = document.querySelectorAll('.text');
-      const lastTextField = textFields[textFields.length - 1];  // 最後尾にあるinput
-  
-      const cloneTextField = lastTextField.cloneNode(true);  // クローンを作成
-      cloneTextField.value = '';  // valueをクリア
-      lastTextField.parentNode.insertBefore(cloneTextField, lastTextField.nextSibling);  // inputを最後尾に追加
-  
-      if (textFields.length >= 1) {
-        btn_remove.style.display = 'inline-block';  // inputが2つ以上あるときに削除ボタンを表示
-      }
-    });
-  
-    // remove
-    btn_remove.addEventListener('click', function() {
-      const textFields = document.querySelectorAll('.text');
-      const lastTextField = textFields[textFields.length - 1];
-      lastTextField.parentNode.removeChild(lastTextField);
-  
-      if (textFields.length <= 2) {
-        btn_remove.style.display = 'none';  // inputが2つ未満のときに削除ボタンを非表示
-      }
-    });
+$(function() {
+
+  // button
+  const btn_clone = $('.btn-add');  // 追加ボタン
+  const btn_remove = $('.btn-remove');  // 削除ボタン
+
+  // clone
+  btn_clone.click(function() {
+    
+    var text = $('.text').last();  // 最後尾にあるinput
+
+		text
+      .clone()  // クローン
+      .val('')  // valueもクローンされるので削除する
+      .insertAfter(text);  // inputを最後尾に追加
+    
+    if ($('.text').length >= 2) {
+      $(btn_remove).show();  // inputが2つ以上あるときに削除ボタンを表示
+    }
+
   });
+
+  // remove
+	btn_remove.click(function() {
   
+    $('.text')
+      .last()
+      .remove();
+
+    if ($('.text').length < 2) {
+      btn_remove.hide();  // inputが2つ未満のときに削除ボタンを非表示
+    }
+    
+  });
+});
